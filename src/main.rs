824d7578c1a518b3c5be74444527e4564d18fd82
Marc Marcos <marcmarcosmadruga@gmail.com>
2025-02-11 18:45:46 +0100

    Inmediate instructions implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use regex::Regex;
+use utils::get_reg_number_from_name;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -22,7 +23,7 @@ fn main() {
 
     for line in contents.lines() {
         // Decode instruction
-        let mut encoded_instruction: String = decode_instruction(line).unwrap();
+        let encoded_instruction: String = decode_instruction(line).unwrap();
         println!("{}: {}", line, encoded_instruction);
     }
 }
@@ -50,10 +51,6 @@ fn decode_instruction(instruction: &str) -> Option<String> {
         "lui" | "auipc" => Some('U'),
         _ => None,
     };
-
-    if instruction_format.is_none() {
-        return None;
-    }
 
     match instruction_format {
         Some('R') => {
@@ -96,15 +93,46 @@ fn decode_instruction(instruction: &str) -> Option<String> {
 
         }
         Some('I') => {
-            let reg = Regex::new(r"t\d+|\d+").unwrap();
+            let reg = Regex::new(r"t\d+").unwrap();
+            let inm = Regex::new(r"\b\d+\b").unwrap();
 
             let result: Vec<String> = reg
                 .find_iter(rest)
                 .map(|mat| mat.as_str().to_string())
                 .collect();
+        
+            let inmediate : Vec<String> = inm
+                .find_iter(rest)
+                .map(|mat| mat.as_str().to_string())
+                .collect();
 
-            for i in result {
-                println!("Register: {:?}", i);
+
+
+            decoded_instruction = match (result.len(), inmediate.len()) {
+                (2, 1) => {
+                    let opcode = 0b0010011;
+                    let rd = get_reg_number_from_name(&result[0]);
+                    let rs1 = get_reg_number_from_name(&result[1]);
+                    let inm = &inmediate[0].parse::<u32>().unwrap();
+
+                    let funct3 = match mnemonic {
+                            "add" => 0x00,
+                            "sub" => 0x0,
+                            "xor" => 0x4,
+                            "or" => 0x6,
+                            "and" => 0x7,
+                            "sll" => 0x1,
+                            "srl" => 0x5,
+                            "sra" => 0x5,
+                            "slt" => 0x2,
+                            "sltu" => 0x3,
+                            &_ => 0
+                    };
+
+                    let final_instruction = (inm << 20) | (rs1 << 15) | (funct3 << 12) | (rd << 7) | (opcode);
+                    format!("{:08x}", final_instruction)
+                }
+                (_, _) => String::from("")
             }
         }
         Some('S') => {
